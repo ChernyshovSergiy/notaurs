12a751339626dbd87c45f9eaa528918c9e33a377
sergey <enebra.org@gmail.com>
2020-04-04 10:55:08 +0300

    redirect after login working correct

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,10 +4,10 @@ const viewsController = require('./../controllers/viewsController');
 const authController = require('./../controllers/authController');
 
 
+router.use(authController.isLoggedIn);
+
 router.get('/', viewsController.getOverview);
 router.get('/login', viewsController.getLoginForm);
-
-router.use(authController.isLoggedIn);
 router.get('/tour/:slug', viewsController.getTour);
 
 
