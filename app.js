cd907351b7cd8235679049987eddd2e884f22c6e
enebra <enebra.org@gmail.com>
2020-04-09 19:46:48 +0300

    Prepared app for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -73,7 +74,7 @@ app.use(
 //   console.log("Hello from the middleware 👋");
 //   next();
 // });
-
+app.use(compression());
 // Test middleware
 app.use((req, res, next) => {
     // req.requestTime = new Date().toISOString();
